e71f2d93127f9304834d7d29c1abdb5a6c74624e
3243239283 <treeclass@qq.com>
2017-08-14 16:19:21 +0800

    vue、vuex、element-ui

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <top></top>
+    <router-view></router-view>
+    <foot></foot>
+  </div>
+</template>
+
+<script>
+import top from './components/header'
+import foot from './components/footer'
+
+export default {
+  name: 'app',
+  components:{
+    top,
+    foot
+  }
+}
+</script>
+
+<style>
+*{
+  padding:0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;  margin:0;
+
+  text-decoration: none;
+}
+ul,li{list-style: none;}
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="footer">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footer',
+  data () {
+    return {
+      msg: 'this is footer'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
