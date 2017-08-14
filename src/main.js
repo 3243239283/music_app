ec02f5a005082263fc4306c9272f65dfeac8661e
3243239283 <treeclass@qq.com>
2017-08-14 16:39:37 +0800

    vue示例代码

---
+++ src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+Vue.config.productionTip = false
+
+//vue-resource
+import vueResource from 'vue-resource'
+Vue.use(vueResource);
+
+// vuex
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+//import '../theme/index.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+Vue.use(ElementUI)
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
